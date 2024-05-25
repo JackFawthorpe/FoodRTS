3adfc24e74b1c7a0f6f69dcaa8ad4db936d42f05
Jack Fawthorpe <fawthorp878@gmail.com>
2024-05-25 21:42:26 +1200

    Coding: Created FoodPile Food Data and Banana Placeholder

---
+++ Game/data/model/food_data.gd
@@ -0,0 +1,8 @@
+class_name FoodData
+extends Resource
+
+@export var name: String
+
+@export var icon: Texture2D
+
+@export var scene:PackedScene
---
+++ Game/scripts/components/food_data.gd
@@ -0,0 +1,5 @@
+class_name FoodPile
+extends Node3D
+
+@export var foodData: FoodData
+@export_range(0, 100) var quantity: int
